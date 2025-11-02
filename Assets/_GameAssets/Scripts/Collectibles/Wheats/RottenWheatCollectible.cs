3262dcd549f46d246e1bd7948a28bb397a6f0948
Hasan Güner <guhasgame@gmail.com>
2025-11-02 17:58:28 +0300

    Scriptable Objects Implemented

--- Assets/_GameAssets/Scripts/Collectibles/Wheats/RottenWheatCollectible.cs
+++ Assets/_GameAssets/Scripts/Collectibles/Wheats/RottenWheatCollectible.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class RottenWheatCollectible : MonoBehaviour, ICollectible
 {
+    [SerializeField] private WheatDesignSO _wheatDesingSO;
     [SerializeField] private PlayerController _playerController;
-    [SerializeField] private float _movementDecreaseSpeed;
-    [SerializeField] private float _resetBoostDuration;
+    
 
 
     public void Collect()
     {
-        _playerController.SetMovementSpeed(_movementDecreaseSpeed, _resetBoostDuration);
+        _playerController.SetMovementSpeed(_wheatDesingSO.IncreaseDecreaseMultiplier, _wheatDesingSO.ResetBoostDuration );
         Destroy(gameObject);
     }
 }
