9e93c317775c94a43f953fa4342039039bd9a7a1
Hasan Güner <guhasgame@gmail.com>
2025-11-02 17:30:49 +0300

    Interface Added to Collectibles

--- Assets/_GameAssets/Scripts/Gameplay/Player/PlayerInteractionController.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Player/PlayerInteractionController.cs
@@ -3,19 +3,12 @@ using UnityEngine;
 
 public class PlayerInteractionController : MonoBehaviour
 {
- private void OnTriggerEnter(Collider other)
- {
-     if(other.CompareTag(Consts.WheatTypes.GOLD_WHEAT))
-     {
-        other.gameObject?.GetComponent<GoldWheatCollectible>().Collect();
-     }
-     if(other.CompareTag(Consts.WheatTypes.HOLY_WHEAT))
-     {
-        other.gameObject?.GetComponent<HolyWheatCollectible>().Collect();
-     }
-     if(other.CompareTag(Consts.WheatTypes.ROTTEN_WHEAT))
-     {
-        other.gameObject?.GetComponent<RottenWheatCollectible>().Collect();
-     }
- }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<ICollectible>(out var collectible))
+        {
+            collectible.Collect();
+        }
+    }
+
 }
