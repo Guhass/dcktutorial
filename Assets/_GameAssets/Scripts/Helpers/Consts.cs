684c7f19b7dc72b4d383ee173900ded7efb10e50
Hasan Güner <guhasgame@gmail.com>
2025-11-11 17:02:30 +0300

    Cat Animations & Lose Situations Added

--- Assets/_GameAssets/Scripts/Helpers/Consts.cs
+++ Assets/_GameAssets/Scripts/Helpers/Consts.cs
@@ -12,13 +12,21 @@ public class Consts
         public const string GROUND_LAYER = "Ground";
         public const string FLOOR_LAYER = "Floor";
     }
-    
+
     public struct PlayerAnimations
     {
         public const string IS_MOVING = "IsMoving";
         public const string IS_SLIDING = "IsSliding";
         public const string IS_JUMPING = "IsJumping";
         public const string IS_SLIDING_ACTIVE = "IsSlidingActive";
+    }
+
+    public struct CatAnimations
+    {
+        public const string IS_IDLING = "IsIdling";
+        public const string IS_WALKING = "IsWalking";
+        public const string IS_RUNNING = "IsRunning";
+        public const string IS_ATTACKING = "IsAttacking";
     }
 
     public struct OtherAnimations
